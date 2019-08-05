32cd8e880684b9380138db373e36d293d39e48bd
BAHAR HAMZA <baharhamza2000@gmail.com>
2019-08-05 15:53:41 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import TaskList from './taskList';
+// import Todoform from './TodoForm';
+class todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <TaskList />
+      </div>
+    );
+  }
+}
+export default todo;
---
+++ src/taskList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import TodoForm from './TodoForm';
+class TaskList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: [],
+      currentTodo: ''
+    };
+  }
+  handleChange = event => {
+    this.setState({ currentTodo: event.target.value });
+  };
+  addItem = event => {
+    event.preventDefault();
+    this.setState({
+      todo: [...this.state.todo, this.state.currentTodo]
+    });
+    event.target.taskName.value = '';
+  };
+  Deletor = event => {
+    // let arr = this.state.todo;
+    // let newA = arr.splice(event.target.value, 1);
+    this.setState({ todo: this.state.todo });
+  };
+  render() {
+    return (
+      <div className='Container'>
+        <TodoForm submitted={this.addItem} change={this.handleChange} />
+        {this.state.todo.map((todo, index) => (
+          <li>
+            {todo}{' '}
+            <button type='button' onClick={this.Deletor} value={index}>
+              Delete
+            </button>{' '}
+          </li>
+        ))}
+      </div>
+    );
+  }
+}
+export default TaskList;
